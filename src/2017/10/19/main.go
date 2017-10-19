35f371bcb8d9e3bdd767d491b3d64afbbb68e4ec
zhangrxiang <599490911@qq.com>
2017-10-19 18:29:22 +0800

    go go go

---
+++ src/2017/10/19/main.go
@@ -0,0 +1,11 @@
+//
+// Created by zhangrongxiang on 2017/10/19 16:46
+// File main
+//
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("Hello, world or 你好，世界 or καλημ ́ρα κóσμ or こんにちはせかい\n")
+}

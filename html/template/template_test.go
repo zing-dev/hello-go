a854e1a377e9638674dee5dbb822c1940e738251
zing <599490911@qq.com>
2019-05-06 13:21:31 +0800

    update template add Funcs

--- html/template/template_test.go
+++ html/template/template_test.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 	"testing"
 )
 
@@ -19,10 +20,48 @@ func TestRun(t *testing.T) {
 			fmt.Println("err :", err)
 		} else {
 			header := writer.Header()
-			header.Set("COntent-Type", "text/html;charset=utf-8")
-			_ = temp.Execute(writer, []string{"hello", "web"})
+			header.Set("Content-Type", "text/html;charset=utf-8")
+			//_ = temp.Execute(writer, []string{"hello", "web"})
+			_ = temp.ExecuteTemplate(writer, "layer", []string{"hello", "web"})
 		}
 
+	})
+
+	http.HandleFunc("/a", func(writer http.ResponseWriter, request *http.Request) {
+
+		temp := template.New("")
+		temp.Funcs(map[string]interface{}{
+			"double": func(str string) string {
+				return strings.Repeat(str, 2)
+			},
+		})
+		var text = `
+<h3>文本</h3>
+<h4>{{.hello | double}} </h2>
+
+<h3>结构体</h3>
+UserName: {{.user.UserName}} <br>
+Address: {{.user.Address}} <br>
+
+<h3>数组</h3>
+{{range $i,$e := .array}}
+	{{$i}} {{$e}}<br>
+{{end}}
+`
+		temp = template.Must(temp.Parse(text))
+		header := writer.Header()
+		header.Set("Content-Type", "text/html;charset=utf-8")
+		_ = temp.Execute(writer, map[string]interface{}{
+			"hello": "hello world ",
+			"user": struct {
+				UserName string
+				Address  string
+			}{
+				UserName: "zing",
+				Address:  "江苏无锡",
+			},
+			"array": [...]int{10, 20, 30, 40, 50, 60},
+		})
 	})
 
 	fmt.Println("开启web服务。。。。。")

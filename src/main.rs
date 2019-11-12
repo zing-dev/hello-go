e6bbc5e93e1324508acba5bd9fd0ec68a51d8003
zing <599490911@qq.com>
2019-11-12 16:56:07 +0800

    add ferris-says

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,11 @@
+use ferris_says::say;
+use std::io::{stdout,BufWriter};
+
 fn main() {
-    println!("Hello, world!");
-    println!("你好世界")
+    let stdout = stdout();
+    let out = b"hello rust!";
+    let width = 24;
+
+    let mut writer = BufWriter::new(stdout.lock());
+    say(out, width, &mut writer).unwrap();
 }

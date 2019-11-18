0cd1e017c7b1193af5c084673f9334a8a8c9c8d3
zing <599490911@qq.com>
2019-11-18 22:17:08 +0800

    fmt

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,74 @@
-use std::fmt;
+use std::fmt::{self, Display, Formatter};
 
-// Import the `fmt` module.
+struct City {
+    name: &'static str,
+    // Latitude
+    lat: f32,
+    // Longitude
+    lon: f32,
+}
 
-// Define a structure named `List` containing a `Vec`.
-struct List(Vec<i32>);
+impl Display for City {
+    // `f` is a buffer, and this method must write the formatted string into it
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        let lat_c = if self.lat >= 0.0 { 'N' } else { 'S' };
+        let lon_c = if self.lon >= 0.0 { 'E' } else { 'W' };
 
-impl fmt::Display for List {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        // Extract the value using tuple indexing,
-        // and create a reference to `vec`.
-        let vec = &self.0;
+        // `write!` is like `format!`, but it will write the formatted string
+        // into a buffer (the first argument)
+        write!(f, "{}: {:.3}°{} {:.3}°{}",
+               self.name, self.lat.abs(), lat_c, self.lon.abs(), lon_c)
+    }
+}
 
-        write!(f, "[")?;
+#[derive(Debug)]
+struct Color {
+    red: u8,
+    green: u8,
+    blue: u8,
+}
 
-        // Iterate over `v` in `vec` while enumerating the iteration
-        // count in `count`.
-        for (count, v) in vec.iter().enumerate() {
-            // For every element except the first, add a comma.
-            // Use the ? operator, or try!, to return on errors.
-            if count != 0 { write!(f, ", ")?; }
-            write!(f, "{}", v)?;
-        }
+#[derive(Debug)]
+struct RGB {
+    red: u8,
+    green: u8,
+    blue: u8,
+}
 
-        // Close the opened bracket and return a fmt::Result value.
-        write!(f, "]\nlength is {}", vec.len())
+impl Display for RGB {
+    // `f` is a buffer, and this method must write the formatted string into it
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        write!(f, "RGB ({},{},{}) 0x{:X}{:X}{:X}",
+               self.red, self.green, self.blue,self.red, self.green, self.blue, )
     }
 }
 
 fn main() {
-    let v = List(vec![1, 2, 3]);
-    println!("{}", v);
+    println!("{}", [1, 2, 3].len());
+    for city in [
+        City { name: "Dublin", lat: 53.347778, lon: -6.259722 },
+        City { name: "Oslo", lat: 59.95, lon: 10.75 },
+        City { name: "Vancouver", lat: 49.25, lon: -123.1 },
+    ].iter() {
+        println!("{},Name is {:?}", *city, city.name);
+    }
+    for color in [
+        Color { red: 128, green: 255, blue: 90 },
+        Color { red: 0, green: 3, blue: 254 },
+        Color { red: 0, green: 0, blue: 0 },
+    ].iter() {
+        // Switch this to use {} once you've added an implementation
+        // for fmt::Display.
+        println!("{:?}", *color);
+    }
+
+    for rgb in [
+        RGB { red: 128, green: 255, blue: 90 },
+        RGB { red: 0, green: 3, blue: 254 },
+        RGB { red: 0, green: 0, blue: 0 },
+    ].iter() {
+        // Switch this to use {} once you've added an implementation
+        // for fmt::Display.
+        println!("{}", *rgb);
+    }
 }

6067b61c8acdffa412ba19fe90e0f1d26138dd77
zing <599490911@qq.com>
2019-11-18 23:07:48 +0800

    array

--- src/main.rs
+++ src/main.rs
@@ -1,77 +1,58 @@
-use std::fmt::{self, Display, Formatter};
+use std::mem;
 
-// Tuples can be used as function arguments and as return values
-fn reverse(pair: (i32, bool)) -> (bool, i32) {
-    // `let` can be used to bind the members of a tuple to variables
-    let (integer, boolean) = pair;
-
-    (boolean, integer)
-}
-
-fn transpose(pair: Matrix) -> Matrix {
-    Matrix(pair.0, pair.2, pair.1, pair.3)
-}
-
-// The following struct is for the activity.
-#[derive(Debug)]
-struct Matrix(f32, f32, f32, f32);
-
-impl Matrix {
-    pub fn transpose(&self) ->Matrix{
-        Matrix(self.0, self.2, self.1, self.3)
-    }
-}
-
-impl Display for Matrix {
-    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
-        write!(f, "({},{})\n({},{})", self.0, self.1, self.2, self.3)
-    }
+// This function borrows a slice
+fn analyze_slice(slice: &[i32]) {
+    println!("first element of the slice: {}", slice[0]);
+    println!("the slice has {} elements", slice.len());
 }
 
 fn main() {
-    // A tuple with a bunch of different types
-    let long_tuple = (1u8, 2u16, 3u32, 4u64,
-                      -1i8, -2i16, -3i32, -4i64,
-                      0.1f32, 0.2f64,
-                      'a', true);
+    // Fixed-size array (type signature is superfluous)
+    let xs: [i32; 5] = [1, 2, 3, 4, 5];
 
-    // Values can be extracted from the tuple using tuple indexing
-    println!("long tuple first value: {}", long_tuple.0);
-    println!("long tuple second value: {}", long_tuple.1);
+    // All elements can be initialized to the same value
+    let ys: [i32; 500] = [0; 500];
 
-    // Tuples can be tuple members
-    let tuple_of_tuples = ((1u8, 2u16, 2u32), (4u64, -1i8), -2i16);
+    // Indexing starts at 0
+    println!("first element of the array: {}", xs[0]);
+    println!("second element of the array: {}", xs[1]);
 
-    // Tuples are printable
-    println!("tuple of tuples: {:?}", tuple_of_tuples);
-    println!("tuple of tuples: {:#?}", tuple_of_tuples);
+    // `len` returns the size of the array
+    println!("array size: {}", xs.len());
 
-    // But long Tuples cannot be printed
-    let too_long_tuple = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13);
-//    println!("too long tuple: {:?}", too_long_tuple);
-    // TODO ^ Uncomment the above 2 lines to see the compiler error
-    let too_long_tuple = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12);
-    println!("too long tuple: {:?}", too_long_tuple);
+    // Arrays are stack allocated
+    println!("array occupies {} bytes", mem::size_of_val(&xs));
 
-    let pair = (1, true);
-    println!("pair is {:?}", pair);
+    // Arrays can be automatically borrowed as slices
+    println!("borrow the whole array as a slice");
+    analyze_slice(&xs);
 
-    println!("the reversed pair is {:?}", reverse(pair));
+    // Slices can point to a section of an array
+    // They are of the form [starting_index..ending_index]
+    // starting_index is the first position in the slice
+    // ending_index is one more than the last position in the slice
+    println!("borrow a section of the array as a slice");
+    analyze_slice(&ys[1..4]);
 
-    // To create one element tuples, the comma is required to tell them apart
-    // from a literal surrounded by parentheses
-    println!("one element tuple: {:?}", (5u32, ));
-    println!("just an integer: {:?}", (5u32));
+    // Out of bound indexing causes compile error
+//    println!("{}", xs[5]);
 
-    //tuples can be destructured to create bindings
-    let tuple = (1, "hello", 4.5, true);
+    let a: [i32; 500] = [1; 500];
+    println!("first element of the slice: {}", a[1]);
+    let mut b = [1; 10];
+    b[0] = 10;
+    println!("first element of the slice: {}", b[0]);
+    println!("first element of the slice: {}", b[1]);
 
-    let (a, b, c, d) = tuple;
-    println!("{:?}, {:?}, {:?}, {:?}", a, b, c, d);
 
-    let matrix = Matrix(1.1, 1.2, 2.1, 2.2);
-    println!("{:?}", matrix);
-    println!("{}", matrix);
-    println!("{}", matrix.transpose());
-    println!("{}", transpose(matrix));
+    for v in b.iter(){
+        println!("value is {}",v);
+    }
+
+    for v in 10..15{
+        println!("value is {}",v);
+    }
+
 }
+
+

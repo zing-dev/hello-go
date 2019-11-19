4af9bb9f702f70649503e5a1de2fa6d76bce5963
zing <599490911@qq.com>
2019-11-19 20:49:07 +0800

    add conversion

---
+++ src/learn/conversion.rs
@@ -0,0 +1,84 @@
+use std::convert::From;
+use std::convert::TryFrom;
+use std::convert::TryInto;
+
+#[derive(Debug)]
+struct Number {
+    value: i32,
+}
+
+impl From<i32> for Number {
+    fn from(item: i32) -> Self {
+        Number { value: item }
+    }
+}
+
+pub fn from_into() {
+    let num = Number::from(30);
+    println!("My number is {:?}", num);
+    println!("My number value is {:?}", num.value);
+
+    let int = 5;
+    // Try removing the type declaration
+    let num: Number = int.into();
+    println!("My number is {:?}", num);
+    println!("My number value is {:?}", num.value);
+}
+
+#[derive(Debug, PartialEq)]
+struct EvenNumber(i32);
+
+impl TryFrom<i32> for EvenNumber {
+    type Error = ();
+
+    fn try_from(value: i32) -> Result<Self, Self::Error> {
+        if value % 2 == 0 {
+            Ok(EvenNumber(value))
+        } else {
+            Err(())
+        }
+    }
+}
+
+pub fn try_from_into() {
+    // TryFrom
+    assert_eq!(EvenNumber::try_from(8), Ok(EvenNumber(8)));
+    assert_eq!(EvenNumber::try_from(5), Err(()));
+
+    match EvenNumber::try_from(8) {
+        Ok(num) => {
+            println!("8是偶数");
+        }
+        Err(_) => {
+            println!("8是奇数");
+        }
+    }
+    // TryInto
+    let result: Result<EvenNumber, ()> = 8i32.try_into();
+    assert_eq!(result, Ok(EvenNumber(8)));
+    let result: Result<EvenNumber, ()> = 5i32.try_into();
+    assert_eq!(result, Err(()));
+}
+
+use std::fmt;
+
+struct Circle {
+    radius: i32
+}
+
+impl fmt::Display for Circle {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Circle of radius {}", self.radius)
+    }
+}
+
+pub fn to_string() {
+    let circle = Circle { radius: 6 };
+    println!("{}", circle.to_string());
+
+    let parsed: i32 = "5".parse().unwrap();
+    let turbo_parsed = "10".parse::<i32>().unwrap();
+
+    let sum = parsed + turbo_parsed;
+    println!("Sum: {:?}", sum);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,25 @@
+#![allow(unused_variables)]
+#![allow(dead_code)]
+
 pub mod learn;
+
+
+fn types() {
+    use learn::types;
+
+    types::literals();
+    types::inference();
+    types::alias()
+}
+
+fn conversion(){
+    use learn::conversion;
+
+    conversion::from_into();
+    conversion::try_from_into();
+    conversion::to_string()
+}
+
 fn main() {
-
-    {
-        use learn::types;
-
-        types::literals();
-        types::inference();
-        types::alias()
-
-
-    }
+    conversion()
 }

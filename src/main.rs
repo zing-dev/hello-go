b36d6f80f6d94e47605d6b61c99f76f80fe169b0
zing <599490911@qq.com>
2019-11-19 16:47:08 +0800

    add variable_bindings

--- src/main.rs
+++ src/main.rs
@@ -1,88 +1,11 @@
-// Create an `enum` to classify a web event. Note how both
-// names and type information together specify the variant:
-// `PageLoad != PageUnload` and `KeyPress(char) != Paste(String)`.
-// Each is different and independent.
-enum WebEvent {
-    // An `enum` may either be `unit-like`,
-    PageLoad,
-    PageUnload,
-    // like tuple structs,
-    KeyPress(char),
-    Paste(String),
-    // or c-like structures.
-    Click { x: i64, y: i64 },
-}
-
-impl WebEvent {
-    fn inspect(&self) {
-        match self {
-            WebEvent::PageLoad => println!("page loaded"),
-            WebEvent::PageUnload => println!("page unloaded"),
-            // Destructure `c` from inside the `enum`.
-            WebEvent::KeyPress(c) => println!("pressed '{}'.", c),
-            WebEvent::Paste(s) => println!("pasted \"{}\".", s),
-            // Destructure `Click` into `x` and `y`.
-            WebEvent::Click { x, y } => {
-                println!("clicked at x={}, y={}.", x, y);
-            }
-        }
-    }
-}
-
-// A function which takes a `WebEvent` enum as an argument and
-// returns nothing.
-fn inspect(event: WebEvent) {
-    match event {
-        WebEvent::PageLoad => println!("page loaded"),
-        WebEvent::PageUnload => println!("page unloaded"),
-        // Destructure `c` from inside the `enum`.
-        WebEvent::KeyPress(c) => println!("pressed '{}'.", c),
-        WebEvent::Paste(s) => println!("pasted \"{}\".", s),
-        // Destructure `Click` into `x` and `y`.
-        WebEvent::Click { x, y } => {
-            println!("clicked at x={}, y={}.", x, y);
-        }
-    }
-}
-
-enum VeryVerboseEnumOfThingsToDoWithNumbers {
-    Add,
-    Subtract,
-}
-
-impl VeryVerboseEnumOfThingsToDoWithNumbers {
-    fn run(&self, x: i32, y: i32) -> i32 {
-        match self {
-            Self::Add => x + y,
-            Self::Subtract => x - y,
-        }
-    }
-}
+pub mod variable;
 
 fn main() {
-    let pressed = WebEvent::KeyPress('x');
-    // `to_owned()` creates an owned `String` from a string slice.
-    let pasted = WebEvent::Paste("my text".to_owned());
-    let click = WebEvent::Click { x: 20, y: 80 };
-    let load = WebEvent::PageLoad;
-    let unload = WebEvent::PageUnload;
-
-    inspect(pressed);
-    inspect(pasted);
-    inspect(click);
-    inspect(load);
-    inspect(unload);
-
-    let mut pasted2 = WebEvent::Paste("my text 2".to_owned());
-    pasted2.inspect();
-    inspect( pasted2);
-
-    type Operations = VeryVerboseEnumOfThingsToDoWithNumbers;
-    let x = Operations::Add;
-
-    println!("{}",x.run(1,2));
-    let sub = VeryVerboseEnumOfThingsToDoWithNumbers::Subtract;
-    println!("{}",sub.run(3,1));
-    println!("{}",VeryVerboseEnumOfThingsToDoWithNumbers::Subtract.run(4,2))
+    {
+        use variable::bindings;
+//        bindings::bindings();
+//        bindings::mut_test();
+//        bindings::scope();
+        bindings::declare();
+    }
 }
-
---
+++ src/variable/bindings.rs
@@ -0,0 +1,89 @@
+pub fn bindings() {
+    let an_integer = 1u32;
+    let a_boolean = true;
+    let unit = ();
+
+    // copy `an_integer` into `copied_integer`
+    let copied_integer = an_integer;
+
+    println!("An integer: {:?}", copied_integer);
+    println!("A boolean: {:?}", a_boolean);
+    println!("Meet the unit value: {:?}", unit);
+
+    // The compiler warns about unused variable bindings; these warnings can
+    // be silenced by prefixing the variable name with an underscore
+    let _unused_variable = 3u32;
+
+    let noisy_unused_variable = 2u32;
+    // FIXME ^ Prefix with an underscore to suppress the warning
+}
+
+pub fn mut_test() {
+    let _immutable_binding = 1;
+    let mut mutable_binding = 1;
+
+    println!("Before mutation: {}", mutable_binding);
+
+    // Ok
+    mutable_binding += 1;
+
+    println!("After mutation: {}", mutable_binding);
+
+    // Error!
+//    _immutable_binding += 1;
+    // FIXME ^ Comment out this line
+}
+
+pub fn scope() {
+    // This binding lives in the main function
+    let long_lived_binding = 1;
+
+    // This is a block, and has a smaller scope than the main function
+    {
+        // This binding only exists in this block
+        let short_lived_binding = 2;
+
+        println!("inner short: {}", short_lived_binding);
+
+        // This binding *shadows* the outer one
+        let long_lived_binding = 5_f32;
+
+        println!("inner long: {}", long_lived_binding);
+    }
+    // End of the block
+
+    // Error! `short_lived_binding` doesn't exist in this scope
+    //println!("outer short: {}", short_lived_binding);
+    // FIXME ^ Comment out this line
+
+    println!("outer long: {}", long_lived_binding);
+
+    // This binding also *shadows* the previous binding
+    let long_lived_binding = 'a';
+
+    println!("outer long: {}", long_lived_binding);
+}
+
+pub fn declare() {
+    // Declare a variable binding
+    let a_binding;
+
+    {
+        let x = 2;
+
+        // Initialize the binding
+        a_binding = x * x;
+    }
+
+    println!("a binding: {}", a_binding);
+
+    let another_binding;
+
+    // Error! Use of uninitialized binding
+    //println!("another binding: {}", another_binding);
+    // FIXME ^ Comment out this line
+
+    another_binding = 1;
+
+    println!("another binding: {}", another_binding);
+}

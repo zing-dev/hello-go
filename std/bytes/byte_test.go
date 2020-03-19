0fa81ae0819a310c29f2279804ec4cd3c65a0d58
zing <599490911@qq.com>
2020-03-19 10:38:47 +0800

    添加excel列号计算方法

--- std/bytes/byte_test.go
+++ std/bytes/byte_test.go
@@ -29,16 +29,6 @@ func TestName3(t *testing.T) {
 	log.Println((91 + 26 - 65) % 26)
 }
 
-func TestName4(t *testing.T) {
-	log.Println(row(65))
-	log.Println(row(66))
-	log.Println(row(90))
-	log.Println(row(91))
-	log.Println(row(92))
-	log.Println(row(93))
-	log.Println(row(93))
-}
-
 func TestName5(t *testing.T) {
 	log.Println(string(65))      //A  65  + 0
 	log.Println(string(66))      //B  65  + 1
@@ -48,13 +38,30 @@ func TestName5(t *testing.T) {
 	log.Println(string(92 + 26)) //AB 65  + 26 + 1
 }
 
-func row(i int) string {
+func TestName4(t *testing.T) {
+	log.Println(row(65))
+	log.Println(row(66))
+	log.Println(row(67))
+	log.Println(row(90))
+	log.Println(row(91))
+	log.Println(row(92))
+	log.Println(row(93))
+	log.Println(row(100))
+	log.Println(row(65 + 26 + 26))
+}
+
+func row(column int) string {
+	column -= 64
 	str := ""
-	i = i - 65
-	for i != 0 {
-		b := i % 26
-		str = string(b+65-1) + str
-		i = i / 26
+	for column > 26 {
+		i := column % 26
+		if i == 0 {
+			str = string(64+26) + str
+			column = (column - 26) / 26
+		} else {
+			str = string(64+i) + str
+			column = (column - i) / 26
+		}
 	}
-	return str
+	return string(column+64) + str
 }

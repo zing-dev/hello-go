0379974914bddf0567aea3791d2748271830041b
zing <599490911@qq.com>
2020-04-03 14:17:34 +0800

    update map

--- std/basic/map.go
+++ std/basic/map.go
@@ -1,10 +1,14 @@
 package basic
 
-import "log"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+)
 
 type Map struct {
-	Id   int
-	Name string
+	Id   int    `json:"id"`
+	Name string `json:"name"`
 }
 
 func map1() {
@@ -43,4 +47,43 @@ func map1() {
 	for _, v := range m2 {
 		log.Println(v.Name)
 	}
+}
+
+func map2() {
+	m := Map{
+		Id:   1,
+		Name: "zing",
+	}
+	data, err := json.Marshal(m)
+	if err != nil {
+		log.Fatal(err)
+	}
+	//{"id":1,"name":"zing"}
+	fmt.Println(string(data))
+	m2 := make(map[string]Map)
+	m2["1"] = m
+	m.Id = 2
+	m2["2"] = m
+	data, err = json.Marshal(m2)
+	if err != nil {
+		log.Fatal(err)
+	}
+	//{"1":{"id":1,"name":"zing"},"2":{"id":2,"name":"zing"}}
+	fmt.Println(string(data))
+
+	m3 := make(map[string]*Map)
+	m3["1"] = &Map{
+		Id:   1,
+		Name: "zing",
+	}
+	m3["2"] = &Map{
+		Id:   2,
+		Name: "zing2",
+	}
+	data, err = json.Marshal(m3)
+	if err != nil {
+		log.Fatal(err)
+	}
+	//{"1":{"id":1,"name":"zing"},"2":{"id":2,"name":"zing2"}}
+	fmt.Println(string(data))
 }

7814b67d9d5dbe294bc523584339edd74cbaa098
zing <599490911@qq.com>
2020-06-08 09:32:01 +0800

    update

--- std/basic/slice_test.go
+++ std/basic/slice_test.go
@@ -1,6 +1,9 @@
 package basic
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestSlice1(t *testing.T) {
 	Slice1()
@@ -12,4 +15,11 @@ func TestSlice2(t *testing.T) {
 
 func TestSlice3(t *testing.T) {
 	Slice3()
+}
+
+func TestSlice4(t *testing.T) {
+	s := make([]int, 0)
+	s = append(s, 1)
+	s = append(s, 2)
+	fmt.Println(s)
 }

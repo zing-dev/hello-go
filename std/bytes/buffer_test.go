5c620ddef2f0272f19948f2896666c796c3aba4e
zing <599490911@qq.com>
2020-07-05 11:03:33 +0800

    update buffer

--- std/bytes/buffer_test.go
+++ std/bytes/buffer_test.go
@@ -33,3 +33,62 @@ func TestGrow(t *testing.T) {
 	log.Println(buffer.Len())
 	log.Println(buffer.Cap())
 }
+
+func TestWrite(t *testing.T) {
+	buffer.Reset()
+	buffer.Write([]byte("hello golang"))
+	log.Println(string(buffer.Bytes()))
+}
+
+func TestWriteString(t *testing.T) {
+	buffer.Reset()
+	buffer.WriteString("golang")
+	log.Println(string(buffer.Bytes()))
+}
+
+func TestReadFrom(t *testing.T) {
+	buf := bytes.NewBufferString("hello golang")
+	buffer.Reset()
+	n, err := buffer.ReadFrom(buf)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(n)
+	log.Println(buffer.String())
+}
+
+func TestWriteTo(t *testing.T) {
+	buf := bytes.Buffer{}
+	n, err := buffer.WriteTo(&buf)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(n)
+	log.Println(buf.String())
+}
+
+func TestWriteByte(t *testing.T) {
+	buffer.Reset()
+	buffer.WriteByte('a')
+	buffer.WriteByte('b')
+	buffer.WriteByte('c')
+	log.Println(buffer.String())
+}
+
+func TestWriteRune(t *testing.T) {
+	buffer.Reset()
+	buffer.WriteRune('哈')
+	buffer.WriteRune('喽')
+	log.Println(buffer.String())
+}
+
+func TestRead(t *testing.T) {
+	buf := make([]byte, buffer.Len())
+	n, err := buffer.Read(buf)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(n)
+	log.Println(buf)
+	log.Println(buffer.Len())
+}

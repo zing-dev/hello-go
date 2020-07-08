8943eec6713f1f8cab3430f4576c3e2aa20bad2c
zing <599490911@qq.com>
2020-07-08 22:30:31 +0800

    update slice

--- std/basic/slice_test.go
+++ std/basic/slice_test.go
@@ -2,6 +2,7 @@ package basic
 
 import (
 	"fmt"
+	"log"
 	"testing"
 )
 
@@ -24,10 +25,37 @@ func TestSlice4(t *testing.T) {
 	fmt.Println(s)
 	Slice4()
 }
+
 func TestSlice5(t *testing.T) {
 	a := make([]int, 3)
 	a[100] = 1
 	a[200] = 1
 	a[300] = 1
 	fmt.Println(a)
+}
+
+func TestSlice6(t *testing.T) {
+	arr := make([]int, 0)
+	for i := 0; i < 10; i++ {
+		arr = append(arr, i)
+	}
+	log.Println(arr)
+	arr = append(arr[:3], arr[4:]...)
+	log.Println(arr)
+	arr = append(arr, 11)
+	log.Println(arr)
+
+	m := map[string][]int{}
+	m["1"] = []int{1, 2, 3}
+	m["1"][0] = 11
+	one := m["1"]
+	one[0] = 12
+	one = []int{}
+
+	m["2"] = arr
+	arr = append(arr[:3], arr[9:]...)
+	arr = m["2"]
+	arr = []int{}
+
+	log.Println(m)
 }

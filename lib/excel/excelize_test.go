ac3033d4335416343cc46913bbcbd5f48530f341
zing <599490911@qq.com>
2020-07-13 19:55:54 +0800

    update excelize

---
+++ lib/excel/excelize_test.go
@@ -0,0 +1,50 @@
+package excel
+
+import (
+	"github.com/360EntSecGroup-Skylar/excelize"
+	"log"
+	"testing"
+)
+
+var f, err = excelize.OpenFile("./data.xlsx")
+
+func TestSheet(t *testing.T) {
+	log.Println(f.GetActiveSheetIndex())
+	f.SetActiveSheet(2)
+	log.Println(f.GetActiveSheetIndex())
+
+	log.Println(f.SheetCount)
+	log.Println(f.GetSheetMap())
+	log.Println(f.GetSheetName(1))
+	log.Println(f.GetSheetIndex(f.GetSheetName(1)))
+	log.Println(f.GetSheetVisible(f.GetSheetName(2)))
+}
+
+func TestRow(t *testing.T) {
+	rows, err := f.Rows(f.GetSheetName(1))
+	if err != nil {
+		log.Fatal(err)
+	}
+	for rows.Next() {
+		columns := rows.Columns()
+		log.Println(columns)
+		for k, v := range columns {
+			log.Println(k, v)
+		}
+	}
+
+	row2 := f.GetRows(f.GetSheetName(1))
+	log.Println(row2)
+	for _, row := range row2 {
+		log.Println(row)
+	}
+}
+
+func TestRead(t *testing.T) {
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(f.GetActiveSheetIndex())
+	m := f.GetSheetMap()
+	log.Println(m)
+}

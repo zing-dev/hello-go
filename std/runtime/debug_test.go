a6728df6b820ecebbca01ea9b4f3c787cc6a8be5
zing <599490911@qq.com>
2020-07-24 10:47:33 +0800

    add bits

---
+++ std/runtime/debug_test.go
@@ -0,0 +1,25 @@
+package runtime
+
+import (
+	"runtime"
+	"testing"
+)
+
+func TestGOMAXPROCS(t *testing.T) {
+	t.Log(runtime.GOMAXPROCS(0))
+	t.Log(runtime.GOMAXPROCS(2))
+	t.Log(runtime.GOMAXPROCS(1))
+}
+
+func TestNumCPU(t *testing.T) {
+	t.Log(runtime.NumCPU())
+}
+
+func TestNumCgoCall(t *testing.T) {
+	t.Log(runtime.NumCgoCall())
+}
+
+func TestNumGoroutine(t *testing.T) {
+	t.Log(runtime.NumGoroutine())
+	runtime.LockOSThread()
+}

d54040c8c4cc62872e4109209ed0a96fb5ae6116
zing <599490911@qq.com>
2021-01-22 11:14:53 +0800

    update swagger

--- lib/iris-swagger/main.go
+++ lib/iris-swagger/main.go
@@ -8,8 +8,8 @@ import (
 	_ "iris-swagger/docs"
 )
 
-// @title swagger
-// @version 1.0
+// @title swagger 接口开发测试
+// @version v0.0.1
 // @description This is a sample server Petstore server.
 // @termsOfService http://swagger.io/terms/
 
@@ -20,6 +20,11 @@ import (
 // @license.name Apache 2.0
 // @license.url http://www.apache.org/licenses/LICENSE-2.0.html
 
+// @tag.name 接口
+// @tag.description 这是接口文档
+
+// @tag.name 接口2
+// @tag.description 这是接口文档2
 //go:generate swag init
 //
 // @host localhost:8080
@@ -28,6 +33,9 @@ func main() {
 	app := iris.New()
 	app.Get("/api/v1/quick/{id}", api.TestQuick)
 	app.Post("/api/v1/quick-post/{id}", api.TestQuickPost)
+	app.Post("/api/v1/quick-post-form/{id}", api.TestQuickPostForm)
+	app.Post("/api/v1/quick-post-query/{id}", api.TestQuickPostQuery)
+	app.Post("/api/v1/quick-file", api.TestQuickFile)
 	app.Get("/api/v1/test/{id}", api.GetStringByInt)
 	app.Get("/swagger/{any:path}", swagger.WrapHandler(swaggerFiles.Handler))
 	app.Run(iris.Addr(":8080"))

d2818d88aa63e3a6bf295f8d93bda231aa1329f1
zing <599490911@qq.com>
2021-03-23 23:56:45 +0800

    add signal

---
+++ std/os/signal_test.go
@@ -0,0 +1,27 @@
+package os
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+	"testing"
+)
+
+func TestSignal(t *testing.T) {
+	sigs := make(chan os.Signal, 1)
+	done := make(chan bool, 1)
+
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		sig := <-sigs
+		fmt.Println()
+		fmt.Println(sig)
+		done <- true
+	}()
+
+	fmt.Println("awaiting signal")
+	<-done
+	fmt.Println("exiting")
+}

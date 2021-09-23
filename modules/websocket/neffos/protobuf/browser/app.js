a6a5ac59d97cb2ea5d6f3730894149c972d1d3da
zing <599490911@qq.com>
2021-09-23 20:09:42 +0800

    add websocket protobuf

---
+++ modules/websocket/neffos/protobuf/browser/app.js
@@ -0,0 +1,86 @@
+const neffos = require('neffos.js');
+const protos = require("./user_message_pb.js")
+
+var scheme = document.location.protocol === "https:" ? "wss" : "ws";
+var port = document.location.port ? ":" + document.location.port : "";
+var wsURL = scheme + "://" + document.location.hostname + port + "/echo";
+
+var outputTxt = document.getElementById("output");
+
+function addMessage(msg) {
+    outputTxt.innerHTML += msg + "\n";
+}
+
+function handleError(reason) {
+    console.log(reason);
+    window.alert(reason);
+}
+
+function handleNamespaceConnectedConn(nsConn) {
+    var username = prompt("Please specify a username: ");
+
+    let inputTxt = document.getElementById("input");
+    let sendBtn = document.getElementById("sendBtn");
+
+    sendBtn.disabled = false;
+    sendBtn.onclick = function () {
+        const input = inputTxt.value;
+        inputTxt.value = "";
+        const userMsg = new protos.UserMessage();
+        userMsg.setUsername(username);
+        userMsg.setText(input);
+        userMsg.setAt(new Date().toString());
+
+        const body = userMsg.serializeBinary()
+        // let msg = new neffos.Message();
+        // msg.Namespace = "default";
+        // msg.Event = "chat";
+        // msg.Body = body;
+        // msg.SetBinary = true;
+        // nsConn.conn.write(msg);
+        // ^ == 
+        nsConn.emitBinary("chat", body);
+        //
+        // OR: javascript side will check if body is binary,
+        // and if it's it will convert it to valid utf-8 text before sending.
+        // To keep the data as they are, please prefer `emitBinary`.
+        // nsConn.emit("chat", body);
+        addMessage("Me: " + input);
+    };
+}
+
+async function runExample() {
+    // You can omit the "default" and simply define only Events, the namespace will be an empty string"",
+    // however if you decide to make any changes on this example make sure the changes are reflecting inside the ../server.go file as well.
+    try {
+        const conn = await neffos.dial(wsURL, {
+            default: { // "default" namespace.
+                _OnNamespaceConnected: function (nsConn, msg) {
+                    addMessage("connected to namespace: " + msg.Namespace);
+                    handleNamespaceConnectedConn(nsConn);
+                },
+                _OnNamespaceDisconnect: function (nsConn, msg) {
+                    addMessage("disconnected from namespace: " + msg.Namespace);
+                },
+                chat: function (nsConn, msg) { // "chat" event.
+                    console.log(msg);
+                    const userMsg = protos.UserMessage.deserializeBinary(msg.Body);
+                    console.log("at: ", userMsg.getAt());
+                    addMessage(userMsg.getUsername() + " at " + userMsg.getAt() + " : " + userMsg.getText());
+                },
+                chat_test: function (nsConn, msg) {
+                    addMessage(new TextDecoder("utf-8").decode(msg.Body));
+                }
+            }
+        });
+
+        await conn.connect("default");
+
+    } catch (err) {
+        handleError(err);
+    }
+}
+
+runExample().then(r => {
+    console.log(r);
+});
---
+++ modules/websocket/neffos/protobuf/go.mod
@@ -0,0 +1,15 @@
+module neffos-protobuf
+
+go 1.17
+
+require (
+	github.com/golang/protobuf v1.5.2
+	github.com/kataras/neffos v0.0.18
+)
+
+require (
+	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/iris-contrib/go.uuid v2.0.0+incompatible // indirect
+	google.golang.org/protobuf v1.26.0 // indirect
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
+)

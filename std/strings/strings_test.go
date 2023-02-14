4bc2027c0be6544cacf7bee1bd071f47d3f6538e
zing <599490911@qq.com>
2023-02-14 16:59:53 +0800

    update string

--- std/strings/strings_test.go
+++ std/strings/strings_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"strings"
 	"testing"
 )
@@ -13,4 +14,12 @@ func NumberStrToSlice(str string) {
 
 func TestNumberStrToSlice(t *testing.T) {
 	NumberStrToSlice("1,2,3,4,5")
+}
+
+func TestChar(t *testing.T) {
+	fmt.Println('P')
+	fmt.Println(string('P'))
+	fmt.Println(string('P') == "P")
+	fmt.Println(string(rune(80)))
+	fmt.Println(string(byte(80)))
 }
